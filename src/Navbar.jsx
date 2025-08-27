e899bde40845f85ee47d93033f8564e6744cb5d1
banshal-Yadav <banshalyadav99@gmail.com>
2025-08-27 16:15:37 +0530

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/"></a>Home</li>
+                <li><a href="/About"></a>About</li>
+                <li><a href="/Contscts"></a>Contacts</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
