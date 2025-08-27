51de741721921c0d9acf2d551787f06f68ac767f
Rajesh Dhoble <rajeshdhoble559@gmail.com>
2025-08-28 00:02:21 +0530

    add: added paragraph below title

--- src/pgTitle.jsx
+++ src/pgTitle.jsx
@@ -4,6 +4,8 @@ function Pgtitle() {
     return (
         <div>
             <h1 > Your AI Career Coach <br />for Professional Sucess</h1>
+             
+            <h4>Your AI career coach that understands your journey <br/>and accelerates your professional growth</h4>
         </div>
     )
 }
