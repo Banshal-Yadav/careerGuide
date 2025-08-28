1c9b21f5209fd6662dd39684ea65f9fd29b7ae66
banshal-Yadav <banshalyadav99@gmail.com>
2025-08-28 22:26:48 +0530

    add: added lucide-reac , used icons for cards  and other fixes

--- src/Cardtab.jsx
+++ src/Cardtab.jsx
@@ -1,13 +1,14 @@
-import Card from './Card.jsx'
-import './Cardtab.css'
+import Card from './Card.jsx';
+import './Cardtab.css';
+import {Brain, MessageSquare, BarChart3, FileText} from 'lucide-react';
 
 function Cardtab() {
     return (
         <div className='card_tab'>
-            <Card icon="icon1" title="AI-Powered Career Guidance"  info="Get personalized career advice and insights powered by advanced Ai technology."/> 
-            <Card icon="icon2" title="Interview preparation"  info="practice with role-specific question and get instant feedback to improve your performance"/> 
-            <Card icon="icon3" title="Industry Insight"  info="Stay ahead with real-time industr trends, salary data, and market analysis."/>
-            <Card icon="icon4" title="Smart Resume Creation"  info="generate ATS-optimized resumes with AI assistance"/>
+            <Card icon={Brain } title="AI-Powered Career Guidance"  info="Get personalized career advice and insights powered by advanced Ai technology."/> 
+            <Card icon={MessageSquare} title="Interview preparation"  info="practice with role-specific question and get instant feedback to improve your performance"/> 
+            <Card icon={BarChart3} title="Industry Insight"  info="Stay ahead with real-time industr trends, salary data, and market analysis."/>
+            <Card icon={FileText} title="Smart Resume Creation"  info="generate ATS-optimized resumes with AI assistance"/>
             
         </div>
     );
