9370661d08bfc4ec25ec46b680349b607aa65309
banshal-Yadav <banshalyadav99@gmail.com>
2025-08-29 22:50:54 +0530

    changelog: Added color variables for text, cards and other components Changes and improved chip structure And minor improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Navbar from './Navbar';
 import Pgtitle from './pgTitle';
-import Button from './Button';
+import GetStartBtn from './GetStartBtn';
 import Chip from "./Chip";
 import Feature from './Feature';
 import Chiptab from './Chiptab';
@@ -10,10 +10,39 @@ function App() {
     <div>
       <Navbar />
       <Pgtitle />
-      <Button />
-      <Chiptab />
-      <Chiptab />
-      <Chiptab />
+      <GetStartBtn />
+      <Chiptab 
+          c1="Machine Learning"
+          c2="Java Devloper"
+          c3="Researcher" 
+          c4="Cloud" 
+          c5="Bigdata" 
+          c6="Ai Engineer" 
+        />
+      <Chiptab 
+          c1="UX Designer" 
+          c2="Creative Director" 
+          c3="Brand Strategist" 
+          c4="Motion Graphics" 
+          c5="Digital Marketing" 
+          c6="Content Creator" 
+        />
+       <Chiptab 
+          c1="Product Manager" 
+          c2="Business Analyst" 
+          c3="Financial Advisor" 
+          c4="Operations" 
+          c5="Strategy" 
+          c6="Team Lead" 
+        />
+        <Chiptab 
+          c1="Data Scientist" 
+          c2="Biomedical Engineer" 
+          c3="Clinical Research" 
+          c4="Healthcare IT" 
+          c5="Medical Writer" 
+          c6="Health Informatics" 
+        />
       <Feature />
     </div>
   );
