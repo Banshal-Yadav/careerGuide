39bdd4af5e6a4dfafab28ceb6470dc1d02bcacba
Rajesh Dhoble <rajesh_r@cmr.edu.in>
2025-08-30 03:09:27 +0530

    add: added footer

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,7 @@ function App() {
         <Stats />
       <Feature  />
       <Footer />
+    
     </div>
   );
 }
--- src/footer.jsx
+++ src/footer.jsx
@@ -1,11 +1,26 @@
 import "./footer.css"
-function Footer(){
+const Footer = () => {
     return(
        <div className="footer">
-        <h3>About  us</h3>
-        <p>@2025 copyright are preserved</p>
+         <div className="ftr-div">
+            <h3 className="info">Home</h3>
+            <p>Skill Assesment </p>
+            <p>Career Advice</p>
+            <p>Learning resources</p>
         </div>
-    );
+         <div className="ftr-div">
+            <h3 className="info">Blogs & Articles</h3>
+            <p> Roadmaps & Guides</p>
+            <p>Interview Tips</p>
+            <p>FAQs</p>
+        </div>
+         <div className="ftr-div">
+            <h3 className="info">Contact Us</h3>
+            <p>support@AIcoachgmail.com </p>
+            <p><a>LinkedIn | Twitter (X) </a></p>
+        </div>
+    </div>
+    );    
     
 }
 export default Footer;
