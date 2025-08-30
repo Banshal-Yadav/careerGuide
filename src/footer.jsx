0a8984664a302694772a1669bae3cc81236c25e5
banshal-Yadav <banshalyadav99@gmail.com>
2025-08-30 13:22:59 +0530

    fixed footer width

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,21 +3,21 @@ const Footer = () => {
     return(
        <div className="footer">
          <div className="ftr-div">
-            <h3 className="info">Home</h3>
-            <p>Skill Assesment </p>
-            <p>Career Advice</p>
-            <p>Learning resources</p>
+            <h3 className="info"> Home</h3>
+            <p> Skill Assesment </p>
+            <p> Career Advice</p>
+            <p> Learning resources</p>
         </div>
          <div className="ftr-div">
             <h3 className="info">Blogs & Articles</h3>
             <p> Roadmaps & Guides</p>
-            <p>Interview Tips</p>
-            <p>FAQs</p>
+            <p> Interview Tips</p>
+            <p> FAQs</p>
         </div>
          <div className="ftr-div">
             <h3 className="info">Contact Us</h3>
-            <p>support@AIcoachgmail.com </p>
-            <p><a>LinkedIn | Twitter (X) </a></p>
+            <p> support@AIcoachgmail.com </p>
+            <p><a> LinkedIn | Twitter (X) </a></p>
         </div>
     </div>
     );    
