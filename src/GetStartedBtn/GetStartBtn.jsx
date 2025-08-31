1006f614994f991712683e783fbfcedc53ee3fe2
banshal-Yadav <banshalyadav99@gmail.com>
2025-08-31 23:16:34 +0530

    minor

--- src/GetStartedBtn/GetStartBtn.jsx
+++ src/GetStartedBtn/GetStartBtn.jsx
@@ -4,7 +4,7 @@ function Button() {
     const navigate = useNavigate();
     return (
         <div className="get-started-btn-container">
-            <button onCanPlay={() => navigate("/AssessmentPg")} className="get-started-btn"><a href=".components/AssessmentPg.jsx">Start Your Assessment</a></button>
+            <button onCanPlay={() => navigate("/AssessmentPg")} className="get-started-btn">Start Your Assessment</button>
         </div>
     );
 }
